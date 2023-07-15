7a79c2024c387e05129c17572a3448781309dd0a
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-07-15 22:03:55 +1000

    Added generateboard function to create valid complete sudoku board

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,4 +1,4 @@
-import { shuffle } from '@/sudoku/sudoku';
+import { boxSafe, generateBoard, printBoard, shuffle } from '@/sudoku/sudoku';
 
 export interface IAppProps {}
 
@@ -6,5 +6,12 @@ export default function App(props: IAppProps) {
   const array = [1, 2, 3, 4, 5];
   console.log(shuffle(array));
 
+  boxSafe([], 2, 53);
+  const board = generateBoard();
+
+  console.log(board);
+  if (board) {
+    printBoard(board);
+  }
   return <div></div>;
 }
--- src/sudoku/sudoku.ts
+++ src/sudoku/sudoku.ts
@@ -1,5 +1,32 @@
+export function solveBoard(board: number[], i: number) {}
+
 export function generateBoard() {
   const board = Array(81).fill(0);
+  return generateBoardFill(board, 0);
+}
+
+function generateBoardFill(board: number[], i: number): number[] | false {
+  if (i > 80) {
+    return board;
+  }
+  const numArray = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  for (let j = 0; j < numArray.length; j++) {
+    if (safeToPlace(board, numArray[j], i)) {
+      board[i] = numArray[j];
+
+      if (generateBoardFill(board, i + 1)) {
+        return board;
+      }
+      console.log(board);
+      board[i] = 0; //return to 0 on backtrack
+    }
+  }
+  return false;
+}
+
+export function safeToPlace(board: number[], num: number, i: number) {
+  return rowSafe(board, num, i) && colSafe(board, num, i) && boxSafe(board, num, i);
 }
 
 export function rowSafe(board: number[], num: number, i: number) {
@@ -17,7 +44,7 @@ export function rowSafe(board: number[], num: number, i: number) {
 export function colSafe(board: number[], num: number, i: number) {
   const col = i % 9;
 
-  for (let j = col; j < 80; j += 9) {
+  for (let j = col; j < 81; j += 9) {
     if (board[j] === num) {
       return false;
     }
@@ -27,18 +54,31 @@ export function colSafe(board: number[], num: number, i: number) {
 }
 
 export function boxSafe(board: number[], num: number, i: number) {
-  const boxRow = Math.floor(i / 9) % 3;
-  const boxCol = i % 3;
+  const boxStartRow = Math.floor(Math.floor(i / 9) / 3) * 3;
+  const boxStartCol = Math.floor((i % 9) / 3) * 3;
+  const boxStartIndex = boxStartRow * 9 + boxStartCol;
 
-  for (let k = boxCol * 3; k < 8; k += 3) {
-    for (let j = boxRow * 3; j < (boxRow + 1) * 3; j++) {
-      if (board[k + j] === num) {
+  console.log(boxStartIndex);
+
+  for (let j = 0; j < 3; j++) {
+    for (let k = 0; k < 3; k++) {
+      if (board[boxStartIndex + 9 * j + k] === num) {
         return false;
       }
     }
   }
-
   return true;
+}
+
+export function printBoard(board: number[]) {
+  for (let k = 0; k < 9; k++) {
+    process.stdout.write('[');
+    for (let j = 0; j < 9; j++) {
+      process.stdout.write(` ${board[k * 9 + j]} `);
+    }
+    process.stdout.write(']');
+    process.stdout.write('\n');
+  }
 }
 
 export function shuffle<T>(array: Array<T>): Array<T> {
