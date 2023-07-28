798bb4cbf192708cfe6985dd86bb82a60d56ece2
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-07-29 05:09:17 +1000

    Added gameStore and outlined the storeState (values and functions)

--- src/store/gameStore/gameStore.ts
+++ src/store/gameStore/gameStore.ts
@@ -1,4 +1,38 @@
+import { create } from 'zustand';
+
 interface IGameState {
+  index: number | undefined;
+
+  playerMoveValidBoard: number[];
+  playerMoveErrorBoard: number[];
   puzzleBoard: number[];
+  hintBoard: number[];
   solutionBoard: number[];
+  notesBoard: TNote[];
+
+  moveHistory: TGameMove[];
+
+  generatePuzzle: () => void;
+  setPostion: (index: number) => void;
+  makeMove: (value: number) => void;
+  makeNote: () => void;
+  erase: () => void;
 }
+
+const useGameStore = create<IGameState>((set) => ({
+  index: undefined,
+  playerMoveValidBoard: Array(81).fill(0),
+  playerMoveErrorBoard: Array(81).fill(0),
+  puzzleBoard: Array(81).fill(0),
+  hintBoard: Array(81).fill(0),
+  solutionBoard: Array(81).fill(0),
+  notesBoard: Array(81),
+
+  moveHistory: [],
+
+  generatePuzzle: () => {},
+  setPostion: (index: number) => {},
+  makeMove: (value: number) => {},
+  makeNote: () => {},
+  erase: () => {},
+}));
--- src/store/gameStore/helper/game.model.ts
+++ src/store/gameStore/helper/game.model.ts
@@ -1,5 +1,19 @@
-interface IGameMove {
-  index: number;
+interface IPlayerMoveError {}
+
+interface IPlayerMoveValid {}
+
+type TGameMove = IPlayerMoveValid | IPlayerMoveError;
+
+type TNote =
+  | {
+      __typename: 'TNote';
+      index: number;
+      values: Set<number>;
+    }
+  | undefined;
+
+interface IPlayerCell {
+  __typename: 'IPlayerCell';
   value: number;
 }
 
@@ -13,9 +27,9 @@ interface ISolutionCell {
   value: number;
 }
 
-interface INoteCell {
-  __typename: 'INoteCell';
-  notes: Set<number>;
+interface IPlayerErrorCell {
+  __typename: 'IPlayerErrorCell';
+  value: number;
 }
 
-type TGameCell = IPuzzleCell | ISolutionCell | INoteCell;
+type TGameCell = IPlayerCell | IPuzzleCell | ISolutionCell | INoteCell;
